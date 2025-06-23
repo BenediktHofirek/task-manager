b517c7f43ae5bbabedc950c0ec4c82c38b1792a5
Benedikt Hofirek <benedikthofirek@gmail.com>
2025-06-23 16:20:06 +0200

    Add Signup and Login page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div>
-      Hello from home!
-      <Link href="/todos" className="text-2xl">Todos</Link>
+    <div className="flex h-full w-full flex-col items-center justify-start pt-[30vh]">
+      <h1 className="text-6xl font-bold">Todo Manager</h1>
+      <div className="mt-16 flex gap-16">
+        <Link
+          href="/signup"
+          className="border-border bg-secondary text-secondary-foreground rounded-3xl border-4 px-6
+            py-4 text-2xl"
+        >
+          Sign up
+        </Link>
+
+        <Link
+          href="/login"
+          className="border-primary bg-primary text-primary-foreground rounded-3xl border-4 px-6 py-4
+            text-2xl"
+        >
+          Log in
+        </Link>
+      </div>
     </div>
   );
 }
